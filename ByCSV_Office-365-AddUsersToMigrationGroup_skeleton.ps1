cf8b1494e20ac1a896b17ba0ab999d7e08b43eaa
PowershellJunkie <86009671+PowershellJunkie@users.noreply.github.com>
2021-06-16 14:52:24 -0400

    Add files via upload

---
+++ ByCSV_Office-365-AddUsersToMigrationGroup_skeleton.ps1
@@ -0,0 +1,13 @@
+﻿$csv = Read-Host "Input full csv file name and path"
+
+
+Import-CSV $csv | ForEach-Object{
+	
+	$user = $_.User
+	
+	Add-ADGroupMember -Identity "<Office 365 Migration group>" -Members $user
+    Add-ADGroupMember -Identity "<Office 365 License group>" -Members $user
+    Add-ADGroupMember -Identity "<your ADFS sync group>" -Members $user
+    Remove-ADGroupMember -Identity "<any group that might break the migration>" -Members $user -Confirm:$False
+	
+}
---
+++ Office-365-AddUsersToMigrationGroup_skeleton.ps1
@@ -0,0 +1,20 @@
+﻿$type = Read-Host "Enter employee type to queue for migration"
+#$OU2 = Read-Host "Department number"
+
+
+$usrArray = @()
+#$usrArraydet = Get-ADUser -SearchBase "OU=$type,OU=$OU2,OU=<your OU>,OU=<your OU>,DC=<your DC>,DC=<your DC>" -Filter * | Where {$_.Enabled -eq $True} | Select-Object sAMAccountName,Mail
+$usrArraydet = Get-ADUser -Filter {employeetype -eq $type}  -Properties sAMAccountName, Mail | Where {$_.Enabled -eq $True} | Select-Object sAMAccountName,Mail
+$usrArray += $usrArraydet 
+
+$usrArray | ForEach-Object{
+	
+	$user = $_.sAMAccountName
+	
+	Add-ADGroupMember -Identity "<Office 365 Migration group>" -Members $user
+    Add-ADGroupMember -Identity "<Office 365 License group>" -Members $user
+    Add-ADGroupMember -Identity "<your ADFS sync group>" -Members $user
+    Remove-ADGroupMember -Identity "<any group that might break the migration>" -Members $user -Confirm:$False
+	
+}
+
