fa3d91a2b523dcb50e25cebe2bacf85d97ff261d
PowershellJunkie <86009671+PowershellJunkie@users.noreply.github.com>
2021-06-16 11:15:25 -0400

    Add files via upload

---
+++ Create-Secure-Credentials.ps1
@@ -0,0 +1,10 @@
+﻿$usracct = (Get-Credential).UserName
+$pass = (Get-Credential).Password
+$loc1 = Read-Host "Input full path to store created credentials encrpyted file"
+$loc2 = Read-Host "Input full path to store created credentials encryption key"
+$PassFile = "$loc1\$usracct.txt"
+$KeyFile = "$loc2\Key_$usracct.key"
+$Key = New-Object Byte[] 32 
+[Security.Cryptopgraphy.RNGCryptoServiceProvider]::Create().GetBytes($Key)
+$Key | Out-File $KeyFile
+$pass | ConvertFrom-SecureString -Key $Key | Out-File $PassFile
